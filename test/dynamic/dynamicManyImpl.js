f4723d623e46680b07aa72366cf990461ce51d0b
Antonio Lain <antlai@cafjs.com>
2014-09-25 23:08:59 -0700

    Testing dynamic supervisor

---
+++ test/dynamic/dynamicManyImpl.js
@@ -0,0 +1,45 @@
+var caf_comp = require('../../index');
+
+var genContainer =  caf_comp.gen_dynamic_container;
+
+/**
+ * Factory method to create a test component.
+ *
+ * @see supervisor
+ */
+exports.newInstance = function($, spec, cb) {
+    try {
+        var that = genContainer.constructor($, spec);
+        var uuid = Math.floor(Math.random() *10000000000000000);
+
+        var time2Die = Math.floor(Math.random() * spec.env.MTBF);
+
+        that.getMessage = function() {
+            return spec.env.message;
+        };
+
+        that.getNumber = function() {
+            return spec.env.number;
+        };
+
+        that.getUUID = function() {
+            return uuid;
+        };
+
+        if (!spec.env.doNotDie) {
+            setTimeout(function() {
+                           var dummyF = function() {
+                               console.log("Dying " + uuid);
+                           };
+                           $._.newFault();
+                           if ($._.stillFaulty()) {
+                               that.__ca_shutdown__(null, dummyF);
+                           }
+                       }, time2Die);
+        }
+        cb(null, that);
+    } catch (err) {
+        console.log('got err' + err);
+        cb(err);
+    }
+};
--- test/test-component.js
+++ test/test-component.js
@@ -2,6 +2,7 @@ var async = require('async');
 var hello = require('./hello/main.js');
 var bye = require('./bye/main.js');
 var faulty = require('./faulty/main.js');
+var dynamic = require('./dynamic/main.js');
 
 exports.helloworld = function (test) {
     test.expect(3);
@@ -165,27 +166,34 @@ exports.properties = function(test) {
                  });
 };
 
+var checkTop = function(test, $) {
+    //10 checks
+    // top component
+    test.equal(typeof($.newHello), 'object',
+               'Cannot create hello');
+    test.ok(!$.newHello.__ca_isShutdown__, 'newHello shutdown');
+
+    // first level
+    var h1 = $.newHello.$.h1;
+    var h2 = $.newHello.$.h2;
+    test.equal(typeof(h1), 'object', 'Cannot create h1');
+    test.equal(typeof(h2), 'object', 'Cannot create h2');
+    test.equal(h1.getMessage(), 'child1');
+    test.equal(h1.getNumber(), 8);
+    test.ok(!h1.__ca_isShutdown__, 'h1 shutdown');
+
+    test.equal(h2.getMessage(), 'child2');
+    test.equal(h2.getNumber(), 9);
+    test.ok(!h2.__ca_isShutdown__, 'h2 shutdown');
+
+};
 var checkSup = function(test, err, $, waitMSec, cb) {
     //23 checks.
     test.ifError(err);
     setTimeout(function() {
-                   // top component
-                   test.equal(typeof($.newHello), 'object',
-                              'Cannot create hello');
-                   test.ok(!$.newHello.__ca_isShutdown__, 'newHello shutdown');
-
-                   // first level
+                   checkTop(test, $);
                    var h1 = $.newHello.$.h1;
                    var h2 = $.newHello.$.h2;
-                   test.equal(typeof(h1), 'object', 'Cannot create h1');
-                   test.equal(typeof(h2), 'object', 'Cannot create h2');
-                   test.equal(h1.getMessage(), 'child1');
-                   test.equal(h1.getNumber(), 8);
-                   test.ok(!h1.__ca_isShutdown__, 'h1 shutdown');
-
-                   test.equal(h2.getMessage(), 'child2');
-                   test.equal(h2.getNumber(), 9);
-                   test.ok(!h2.__ca_isShutdown__, 'h2 shutdown');
 
                    // second level
                    var h21 = h2.$.h21;
@@ -211,17 +219,124 @@ var checkSup = function(test, err, $, waitMSec, cb) {
 };
 exports.supervisor = function(test) {
     test.expect(24);
-//    test.expect(2);
     var context;
     async.series([
                      function(cb) {
                          faulty.load(null, {name: 'newHello'}, 'faulty1.json',
                                      null, function(err, $) {
                                          context = $;
-                                         console.log(err);
                                          checkSup(test, err, $, 10000, cb);
-//                                         cb(null);
                                      });
+                     },
+                     function(cb) {
+                         context.newHello.__ca_shutdown__(null, cb);
+                     }
+                 ], function(err, data) {
+                     test.ifError(err);
+                     test.done();
+                 });
+};
+
+
+
+var MTBF = 500;
+
+var specBaseDyn = function(name) {
+    return {
+        module: "./dynamicImpl",
+        name: name,
+        env : {
+            message: name + '_msg',
+            number:42,
+            MTBF: MTBF
+        }
+    };
+};
+
+var specTempDyn = function(name) {
+    var res = specBaseDyn(name);
+    res.env.__ca_temporary__= true;
+    return res;
+};
+
+
+var specDyn = function(name) {
+    return ((name.indexOf('temp_') === 0) ?
+            specTempDyn(name) : specBaseDyn(name));
+};
+
+var DYN_ADD = ['temp_comp1', 'comp2', 'comp3', 'temp_comp4', 'temp_comp5',
+               'comp6',  'comp7', 'temp_comp8','comp9', 'comp10'];
+
+var DYN_RM = ['temp_comp5', 'comp6', 'comp9'];
+
+var DYN_LEFT = {'comp2': true, 'comp3': true, 'comp7': true, 'comp10': true};
+
+var checkDyn = function(test, err, $, waitMSec, cb) {
+    //13 checks.
+    test.ifError(err);
+    setTimeout(function() {
+                   checkTop(test, $);
+                   var h2 = $._.$.h2;
+                   var expected = Object.keys(DYN_LEFT).sort();
+                   var actual = Object.keys(h2.$)
+                       .filter(function(x) { return x !== '_';})
+                       .sort();
+                   test.deepEqual(actual, expected,
+                                  'Unexpected dynamic components');
+                   var errorKey = null;
+                   actual.forEach(function(x) {
+                                      if (typeof(h2.$[x]) !== 'object') {
+                                          console.log('error:not an object');
+                                          errorKey = x;
+                                      } else if (h2.$[x].getMessage() !==
+                                                 x + '_msg') {
+                                          console.log('error:wrong msg ' +
+                                                     h2.$[x].getMessage());
+                                          errorKey = x;
+                                      } else if (h2.$[x].__ca_isShutdown__) {
+                                          console.log('error:shutdown');
+                                          errorKey = x;
+                                      }
+                                  });
+                   test.ok(errorKey === null, 'Error with dyn comp ' +
+                           errorKey);
+                   cb(null);
+               }, waitMSec);
+};
+
+
+exports.dynamic = function(test) {
+    test.expect(14);
+    var context;
+    var h2;
+    async.series([
+                     function(cb) {
+                         dynamic.load(null, {name: 'newHello'}, 'dynamic1.json',
+                                      null, function(err, $) {
+                                          if (err) {
+                                              cb(err);
+                                          } else {
+                                              context = $;
+                                              h2 = $._.$.h2;
+                                              cb(err, $);
+                                          }
+                                      });
+                     },
+                     function(cb) {
+                         async.eachSeries(DYN_ADD, function(x, cb1) {
+                                              h2.__createChild__(null,
+                                                                 specDyn(x),
+                                                                 cb1);
+                                          }, cb);
+                     },
+                     function(cb) {
+                         async.eachSeries(DYN_RM, function(x, cb1) {
+                                              h2.__deleteChild__(null, x, cb1);
+                                          }, cb);
+                     },
+                     function(cb) {
+                         checkDyn(test, null, context, 10000, cb);
                      },
                      function(cb) {
                          context.newHello.__ca_shutdown__(null, cb);
